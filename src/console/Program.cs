b2be27de947771a4ebf2549b6b95c1349b9e6c70
kazenetu <rakudafactory@gmail.com>
2022-08-30 23:17:58 +0900

    仮実装からJson解析クラス使用に切り替え

--- src/console/Program.cs
+++ src/console/Program.cs
@@ -22,11 +22,8 @@ internal class Program
         }";
 
         Console.WriteLine("--単純なJSON文字列--");
-        ProcessJsonDocument(json);
-        Console.WriteLine("--------------------");
         ShowJsonResult(json);
         Console.WriteLine("--------------------");
-
         // 出力：
         // --単純なJSON文字列--
         // String propString
@@ -47,14 +44,11 @@ internal class Program
         }";
 
         Console.WriteLine("--Objectが含まれるJSON文字列--");
-        ProcessJsonDocument(json);
-        Console.WriteLine("--------------------");
         ShowJsonResult(json);
         Console.WriteLine("--------------------");
         // --Objectが含まれるJSON文字列--
         // Object propObjct
         //   String propObjString
-        //   
         // Number propNumber
 
         // Object配列が含まれるJSON文字列作成
@@ -71,8 +65,6 @@ internal class Program
         }";
 
         Console.WriteLine("--Object配列が含まれるJSON文字列--");
-        ProcessJsonDocument(json);
-        Console.WriteLine("--------------------");
         ShowJsonResult(json);
         Console.WriteLine("--------------------");
         // --Object配列が含まれるJSON文字列--
@@ -84,123 +76,5 @@ internal class Program
     {
         var result = new JsonParser(json);
         Console.WriteLine(result.Result);
-    }
-
-    /// <summary>
-    /// JsonDocumentで構造を解析する
-    /// </summary>    
-    /// <param name="json">JSON文字列</param>
-    private static void ProcessJsonDocument(string json)
-    {
-        var innerProperties = new StringBuilder();
-        var jsonDocument = JsonDocument.Parse(json);
-        var rootElement = jsonDocument.RootElement;
-        foreach (var element in rootElement.EnumerateObject())
-        {
-            // 型を特定する
-            var propertyData = GetPropertyNameAndKind(element.Value);
-
-            // 追加の処理を入れる
-            switch(propertyData.valueKind)
-            {
-                case JsonValueKind.String:
-                    if(DateTime.TryParse(element.Value.ToString(), out var _))
-                    {
-                        propertyData.kindName = "DateTime";
-                    }
-                    break;
-                case JsonValueKind.Array:
-                    var addedItem = new List<string>();
-                    var arrayType = string.Empty;
-                    var arrayIndex = 0;
-                    while(arrayIndex < element.Value.GetArrayLength())
-                    {
-                        if(string.IsNullOrEmpty(arrayType) || arrayType == element.Value[arrayIndex].ValueKind.ToString())
-                        {
-                            var (kindName,ValueKind) = GetPropertyNameAndKind(element.Value[arrayIndex]);
-                            arrayType = kindName;
-
-                            if(ValueKind == JsonValueKind.Object)
-                            {
-                                foreach (var objElement in element.Value[arrayIndex].EnumerateObject())
-                                {
-                                    var (ekindName,eValueKind) = GetPropertyNameAndKind(objElement.Value);
-                                    var prop = $"  {ekindName} {objElement.Name}";
-                                    if(addedItem.Contains(prop))
-                                    {
-                                        continue;
-                                    }
-                                    innerProperties.AppendLine(prop);
-
-                                    addedItem.Add(prop);
-                                }
-                            }
-                        }
-                        else
-                        {
-                            arrayType = "etc";
-                        }
-                        arrayIndex++;
-                    }
-                    if(string.IsNullOrEmpty(arrayType))
-                    {
-                        arrayType = "noting...";
-                    }
-                    propertyData.kindName += $"({arrayType})";
-                    break;
-                case JsonValueKind.Object:
-                    foreach (var objElement in element.Value.EnumerateObject())
-                    {
-                        var (kindName,ValueKind) = GetPropertyNameAndKind(objElement.Value);
-                        innerProperties.AppendLine($"  {kindName} {objElement.Name}");
-                    }
-                    break;
-            }
-            Console.WriteLine($"{propertyData.kindName} {element.Name}");
-            if(innerProperties.Length > 0)
-            {
-                Console.WriteLine(innerProperties.ToString());
-            }
-            innerProperties.Clear();
-        }
-    }
-
-    /// <summary>
-    /// JsonElementの型名とJsonValueKindを取得する
-    /// </summary>    
-    /// <param name="src">対象インスタンス</param>
-    /// <returns>型名とJsonValueKindのタプル</returns>
-    private static (string kindName, JsonValueKind valueKind) GetPropertyNameAndKind(JsonElement src)
-    {
-        // 型を特定する
-        var valueKind = "none...";
-        switch(src.ValueKind)
-        {
-            case JsonValueKind.String:
-                valueKind = "String";
-                break;
-            case JsonValueKind.Array:
-                valueKind = "Array";
-                break;
-            case JsonValueKind.Number:
-                valueKind = "Number";
-                break;
-            case JsonValueKind.Object:
-                valueKind = "Object";
-                break;
-            case JsonValueKind.Undefined:
-                valueKind = "Undefined";
-                break;
-            case JsonValueKind.True:
-                valueKind = "True";
-                break;
-            case JsonValueKind.False:
-                valueKind = "False";
-                break;
-            case JsonValueKind.Null:
-                valueKind = "Null";
-                break;
-        }
-        return (valueKind, src.ValueKind);
     }
 }
