8690d0084fbdc5acb87c25e21922f73be899623f
kazenetu <rakudafactory@gmail.com>
2022-09-06 23:22:38 +0900

    クラス情報を追加

---
+++ src/console/Common/Class.cs
@@ -0,0 +1,57 @@
+using System.Collections.ObjectModel;
+
+  /// <summary>
+  /// クラス情報
+  /// </summary>
+  public class Class
+  {
+    /// <summary>
+    /// 名称
+    /// </summary>
+    /// <value>プロパティ名</value>
+    public string Name { get; init; }
+
+    /// <summary>
+    /// コメント
+    /// </summary>
+    /// <value>コメント文字列</value>
+    public string Comment { get; init; }
+
+    /// <summary>
+    /// プロパティリスト
+    /// </summary>
+    /// <returns>プロパティリスト</returns>
+    public ReadOnlyCollection<Property> Properties { get; init; }
+
+    /// <summary>
+    /// インナークラスリスト
+    /// </summary>
+    /// <returns>プロパティリスト</returns>
+    public List<Class> InnerClass { get; init; }
+
+    /// <summary>
+    /// 非公開コンストラクタ
+    /// </summary>
+    private Class()
+    {
+        Name = string.Empty;
+        Comment = string.Empty;
+    }
+
+    /// <summary>
+    /// インスタンス生成
+    /// </summary>
+    /// <param name="name">クラス名称</param>
+    /// <param name="comment">コメント文字列</param>
+    /// <param name="properties">プロパティリスト</param>
+    /// <returns>クラスエンティティ インスタンス</returns>
+    public static Class Create(string name, string comment, ReadOnlyCollection<Property> properties)
+    {
+      return new Class()
+      {
+        Name = name,
+        Comment = comment,
+        Properties = properties
+      };
+    }
+  }
