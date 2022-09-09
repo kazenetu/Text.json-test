579960fe6bc6cad871c2b34f3d3efc32d059a892
kazenetu <rakudafactory@gmail.com>
2022-09-09 22:22:37 +0900

    単純なJSON文字列デシリアライズ用クラスを追加

---
+++ src/console/DeserializeSamples/SimpleJson.cs
@@ -0,0 +1,36 @@
+using System.Text;
+
+/// <summary>
+/// 単純なJSON文字列デシリアライズ用クラス
+/// </summary>
+public class SimpleJson
+{
+    public string propString { set; get; } = string.Empty;
+
+    public decimal propNumber { set; get; }
+
+    public string propDate { set; get; } = string.Empty;
+
+    public bool propTrue { set; get; }
+
+    public bool propFalse { set; get; }
+
+    public object propNull { set; get; }  = string.Empty;
+
+    public List<decimal> propArray { set; get; } = new List<decimal> ();
+
+    public override string ToString()
+    {
+        var result = new StringBuilder();
+
+        result.AppendLine($"propString:{propString}");
+        result.AppendLine($"propNumber:{propNumber}");
+        result.AppendLine($"propDate:{propDate}");
+        result.AppendLine($"propTrue:{propTrue}");
+        result.AppendLine($"propFalse:{propFalse}");
+        result.AppendLine($"propNull:{propNull}");
+        result.AppendLine($"propArray:[{string.Join(",",propArray)}]");
+
+        return result.ToString();
+    }
+}
