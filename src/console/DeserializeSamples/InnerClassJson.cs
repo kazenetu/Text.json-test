c7b05ae3b69fb1af90950bd2eb07d47ea1f6611f
kazenetu <rakudafactory@gmail.com>
2022-09-12 23:02:52 +0900

    bjectが含まれるJSON文字列デシリアライズ用クラスを追加

---
+++ src/console/DeserializeSamples/InnerClassJson.cs
@@ -0,0 +1,38 @@
+using System.Text;
+/// <summary>
+/// Objectが含まれるJSON文字列デシリアライズ用クラス
+/// </summary>
+public class InnerClassJson
+{
+    /// <summary>
+    /// Object用クラス
+    /// </summary>
+    public class InnerClass
+    {
+        public string propObjString{set;get;}=string.Empty;
+
+        public override string ToString()
+        {
+            var result = new StringBuilder();
+
+            result.AppendLine($"InnerClass");
+            result.AppendLine($"  propObjString:{propObjString}");
+
+            return result.ToString();
+        }
+    }
+
+    public InnerClass? propObjct {set;get;}
+
+    public Decimal propNumber{set;get;}
+
+    public override string ToString()
+    {
+        var result = new StringBuilder();
+
+        result.Append($"propObjct:{propObjct}");
+        result.AppendLine($"propNumber:{propNumber}");
+
+        return result.ToString();
+    }
+}
