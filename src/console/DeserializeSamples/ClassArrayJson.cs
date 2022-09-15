5052833d5fff0bd15ee61ca6d1e7eecb362af7a9
kazenetu <rakudafactory@gmail.com>
2022-09-15 22:24:17 +0900

    「Object配列が含まれるJSONデシリアライズ」用クラス追加

---
+++ src/console/DeserializeSamples/ClassArrayJson.cs
@@ -0,0 +1,45 @@
+using System.Text;
+
+/// <summary>
+/// Object配列が含まれるJSONデシリアライズ用クラス
+/// </summary>
+public class ClassArrayJson
+{
+    /// <summary>
+    /// Object用クラス
+    /// </summary>
+    public class InnerClass
+    {
+        public string propObjString{set;get;}=string.Empty;
+
+        public override string ToString()
+        {
+            var result = new StringBuilder();
+
+            result.Append("  InnerClass...");
+            result.AppendLine($"  propObjString:{propObjString}");
+
+            return result.ToString();
+        }
+    }
+
+    public List<InnerClass>? propObjcts {set;get;}
+
+    public override string ToString()
+    {
+        var result = new StringBuilder();
+
+        if(propObjcts is null){
+            result.AppendLine($"propObjcts is null");
+            return result.ToString();
+        }
+
+        result.AppendLine($"propObjcts");
+        foreach (var item in propObjcts)
+        {
+            result.Append(item);
+        }
+
+        return result.ToString();
+    }
+}
