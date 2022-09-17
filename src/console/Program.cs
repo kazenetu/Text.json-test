5fc11e2afddb9f6ebcabf6700f85b24b82c1dafe
kazenetu <rakudafactory@gmail.com>
2022-09-17 21:54:34 +0900

    「Object配列が含まれるJSONのデシアライズ結果のコメント追加

--- src/console/Program.cs
+++ src/console/Program.cs
@@ -98,6 +98,11 @@ internal class Program
         Console.WriteLine("----Deserialize結果----");
         Console.WriteLine(classArrayJson?.ToString());
         Console.WriteLine("--------------------");
+        // 出力結果：
+        // ----Deserialize結果----
+        // propObjcts
+        //   InnerClass...  propObjString:propObjString1
+        //   InnerClass...  propObjString:propObjString2
 
         // Objectのネストを含むJSON文字列作成
         json = @"{
