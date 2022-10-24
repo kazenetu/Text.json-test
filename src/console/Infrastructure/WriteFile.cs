34bc6ba79334c783f6aea6337a8f5b688a1e8b4a
kazenetu <rakudafactory@gmail.com>
2022-10-24 22:29:44 +0900

    [WIP]ファイル出力クラスを追加 #2

---
+++ src/console/Infrastructure/WriteFile.cs
@@ -0,0 +1,19 @@
+/// <summary>
+/// ファイル出力クラス
+/// </summary>
+public class WriteFile : IOutput
+{
+    /// <summary>
+    /// ファイルを出力する
+    /// </summary>
+    /// <param name="outputData">出力データ</param>
+    /// <param name="filePath">出力ファイルパス</param>
+    /// <param name="nameSpace">名前空間</param>
+    /// <returns>出力結果</returns>
+    public bool OutputFile(String outputData, String filePath, String nameSpace = "")
+    {
+        //TODO ファイル出力
+
+        return false;
+    }
+}
