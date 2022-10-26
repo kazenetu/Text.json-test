9816e18d4422be5b64967521eb6184b8e552fec3
kazenetu <rakudafactory@gmail.com>
2022-10-26 23:44:15 +0900

    ファイル出力メソッドの見直し #2

--- src/console/Common/IOutput.cs
+++ src/console/Common/IOutput.cs
@@ -6,9 +6,9 @@ public interface IOutput
     /// <summary>
     /// ファイルを出力する
     /// </summary>
-    /// <param name="outputData">出力データ</param>
+    /// <param name="classInstance">クラスエンティティ</param>
     /// <param name="filePath">出力ファイルパス</param>
     /// <param name="nameSpace">名前空間</param>
     /// <returns>出力結果</returns>
-    bool OutputFile(String outputData, String filePath, String nameSpace = "");
+    bool Output(Class classInstance, String? rootPath= null, String? nameSpace = null);
 }
--- src/console/Infrastructure/WriteFile.cs
+++ src/console/Infrastructure/WriteFile.cs
@@ -8,22 +8,23 @@ public class WriteFile : IOutput
     /// <summary>
     /// ファイルを出力する
     /// </summary>
-    /// <param name="outputData">出力データ</param>
+    /// <param name="classInstance">クラスエンティティ</param>
     /// <param name="filePath">出力ファイルパス</param>
     /// <param name="nameSpace">名前空間</param>
     /// <returns>出力結果</returns>
-    public bool OutputFile(String outputData, String filePath, String nameSpace = "")
+    public bool Output(Class classInstance, String? rootPath= null, String? nameSpace = null)
     {
         // フォルダの存在確認とフォルダ作成
-        if (!Directory.Exists(filePath))
+        if (!Directory.Exists(rootPath))
         {
-            Directory.CreateDirectory(filePath);
+            Directory.CreateDirectory(rootPath);
         }
+        var filePath = Path.Combine(rootPath,$"{classInstance.Name}.cs");
 
         // ファイル出力
         using (FileStream fs = File.OpenWrite(filePath))
         {
-            Byte[] info = new UTF8Encoding(true).GetBytes(outputData);
+            Byte[] info = new UTF8Encoding(true).GetBytes(classInstance.ToString(0));
             fs.Write(info, 0, info.Length);
         }
 
