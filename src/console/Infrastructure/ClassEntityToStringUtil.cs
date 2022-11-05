36fdcdd145bdbd3fdb2d3e2185d908f107d95dc5
kazenetu <rakudafactory@gmail.com>
2022-11-05 17:54:07 +0900

    ユーティリティクラスのコメント追加 #2

--- src/console/Infrastructure/ClassEntityToStringUtil.cs
+++ src/console/Infrastructure/ClassEntityToStringUtil.cs
@@ -1,24 +1,34 @@
 using System.Text;
 
+/// <summary>
+/// クラスエンティティからclass文字列を作成するユーティリティクラス
+/// </summary>
 public static class ClassEntityToStringUtil
 {
-    public static string GetClassString(Class classEntity, int index = 0)
+    /// <summary>
+    /// クラスエンティティからclass文字列生成して返す
+    /// </summary>
+    /// <param name="classEntity">クラスエンティティインスタンス</param>
+    /// <param name="indentLevel">インデントレベル</param>
+    /// <returns>class文字列</returns>
+    public static string GetClassString(Class classEntity, int indentLevel = 0)
     {
         var result = new StringBuilder();
 
-        var levelSpace = new string('S', index).Replace("S", "  ");
+        // インデント設定
+        var levelSpace = new string('S', indentLevel).Replace("S", "  ");
         result.AppendLine($"{levelSpace}public class {classEntity.Name} {{");
 
-        // クラス
+        // クラス文字列作成
         foreach (var classInstance in classEntity.InnerClass)
         {
-            result.AppendLine($"{levelSpace}{GetClassString(classInstance,index + 1)}");
+            result.AppendLine($"{levelSpace}{GetClassString(classInstance,indentLevel + 1)}");
         }
 
-        // プロパティ
+        // プロパティ文字列作成
         foreach (var property in classEntity.Properties)
         {
-            result.Append($"{levelSpace}{GetPropertyString(property,index + 1)}");
+            result.Append($"{levelSpace}{GetPropertyString(property,indentLevel + 1)}");
         }
 
         result.AppendLine($"{levelSpace}}}");
@@ -26,17 +36,27 @@ public static class ClassEntityToStringUtil
         return result.ToString();
     }
 
-    private static string GetPropertyString(Property propertyEntity, int index)
+    /// <summary>
+    /// プロパティエンティティからプロパティ文字列を作成して返す
+    /// </summary>
+    /// <param name="propertyEntity">プロパティエンティティインスタンス</param>
+    /// <param name="indentLevel">インデントレベル</param>
+    /// <returns>プロパティ文字列</returns>
+    private static string GetPropertyString(Property propertyEntity, int indentLevel)
     {
         var result = new StringBuilder();
 
+        // デフォルト設定
         var defaultValue = string.Empty;
         if (propertyEntity.TypeName == "string" || propertyEntity.TypeName == "object")
         {
             defaultValue = "string.Empty";
         }
 
-        var levelSpace = new string('S', index).Replace("S", "  ");
+        // インデント設定
+        var levelSpace = new string('S', indentLevel).Replace("S", "  ");
+
+        // プロパティ文字列作成
         result.Append($"{levelSpace}public {propertyEntity.TypeName} {propertyEntity.Name}{{set; get;}}");
         if (!string.IsNullOrEmpty(defaultValue))
         {
