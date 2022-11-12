be2c5783c767ea0960216d07cfe728f4d09c72dc
kazenetu <rakudafactory@gmail.com>
2022-11-12 23:48:18 +0900

    インターフェースのパラメータを変更 #3

--- src/console/Common/IConsoleOutputRepository.cs
+++ src/console/Common/IConsoleOutputRepository.cs
@@ -7,6 +7,7 @@ public interface IConsoleOutputRepository
     /// コンソール出力する
     /// </summary>
     /// <param name="classInstance">クラスエンティティ</param>
+    /// <param name="args">パラメータリスト</param>
     /// <returns>出力結果</returns>
-    bool Output(Class classInstance);
+    bool Output(Class classInstance, List<(string key,string value)> args);
 }
--- src/console/Common/IFileOutputRepository.cs
+++ src/console/Common/IFileOutputRepository.cs
@@ -4,16 +4,10 @@
 public interface IFileOutputRepository
 {
     /// <summary>
-    /// ファイルを出力する
-    /// </summary>
-    /// <param name="filePath">出力ファイルパス</param>
-    /// <param name="nameSpace">名前空間</param>
-    void Initialize(string? rootPath = null, string? nameSpace = null);
-
-    /// <summary>
     /// ファイル出力する
     /// </summary>
     /// <param name="classInstance">クラスエンティティ</param>
+    /// <param name="args">パラメータリスト</param>
     /// <returns>出力結果</returns>
-    bool Output(Class classInstance);
+    bool Output(Class classInstance, List<(string key,string value)> args);
 }
