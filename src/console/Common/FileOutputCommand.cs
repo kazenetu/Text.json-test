a164205dc278eadc7603707be6233ab791bc07b1
kazenetu <rakudafactory@gmail.com>
2022-11-13 19:23:18 +0900

    ファイル出力コメントクラス追加 #3

---
+++ src/console/Common/FileOutputCommand.cs
@@ -0,0 +1,26 @@
+/// <summary>
+/// ファイル出力コマンドクラス
+/// </summary>
+public class FileOutputCommand
+{
+    /// <summary>
+    /// ファイル出力のルートパス
+    /// </summary>
+    public string RootPath {get; init;}
+
+    /// <summary>
+    /// 名前空間
+    /// </summary>
+    public string NameSpace {get; init;}
+
+    /// <summary>
+    /// コンストラクタ
+    /// </summary>
+    /// <param name="rootPath">ファイル出力のルートパス</param>
+    /// <param name="nameSpace">名前空間</param>
+    public FileOutputCommand(string rootPath, string nameSpace)
+    {
+        RootPath = rootPath;
+        NameSpace = nameSpace;
+    }
+}
