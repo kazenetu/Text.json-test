468d42078e0c4c4a2330f17c868f15a7be17929f
kazenetu <rakudafactory@gmail.com>
2022-11-22 22:58:09 +0900

    戻り値のミスを修正 #4

--- src/console/Common/IJsonRepository.cs
+++ src/console/Common/IJsonRepository.cs
@@ -7,13 +7,13 @@ public interface IJsonRepository
     /// JSONファイルを読み込んでClass情報を返す
     /// </summary>
     /// <param name="filePath">JSONファイル</param>
-    /// <returns>Class文字列</returns>
+    /// <returns>Classエンティティ</returns>
     Class ReadFromFile(string filePath);
 
     /// <summary>
     /// JSON文字列を読み込んでClass情報を返す
     /// </summary>
     /// <param name="filePath">JSO文字列</param>
-    /// <returns>Class文字列</returns>
+    /// <returns>Classエンティティ</returns>
     Class ReadFromString(string json);
 }
