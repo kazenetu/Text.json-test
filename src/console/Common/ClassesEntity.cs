b5f12e76f82229c2ac007e00f1ffafb8e7c7e664
kazenetu <rakudafactory@gmail.com>
2022-12-04 23:22:45 +0900

    [WIP]class文字列出力機能を仮実装 #7

--- src/console/Common/ClassesEntity.cs
+++ src/console/Common/ClassesEntity.cs
@@ -1,3 +1,5 @@
+using System.Text;
+
 /// <summary>
 /// クラス集約エンティティ
 /// </summary>
@@ -41,4 +43,87 @@ public class ClassesEntity
             InnerClasses = innerClasses
         };
     }
+
+    #region class文字列作成
+
+    /// <summary>
+    /// class文字列生成して返す
+    /// </summary>
+    public string GetClassString()
+    {
+        var result = string.Empty;
+
+        //HACK 名前空間の設定
+
+        // ルートクラスを出力
+        result += GetClassString(RootClass, 0);
+
+
+        return result;
+    }
+
+    /// <summary>
+    /// クラスエンティティからclass文字列生成して返す
+    /// </summary>
+    /// <param name="classEntity">クラスエンティティインスタンス</param>
+    /// <param name="indentLevel">インデントレベル</param>
+    /// <returns>class文字列</returns>
+    private string GetClassString(Class classEntity, int indentLevel = 0)
+    {
+        var result = new StringBuilder();
+
+        // インデント設定
+        var levelSpace = new string('S', indentLevel).Replace("S", "  ");
+        result.AppendLine($"{levelSpace}public class {classEntity.Name}");
+        result.AppendLine($"{levelSpace}{{");
+
+        // クラス文字列作成
+        foreach (var classInstance in InnerClasses)
+        {
+            result.AppendLine($"{GetClassString(classInstance, indentLevel + 1)}");
+        }
+
+        // プロパティ文字列作成
+        foreach (var property in classEntity.Properties)
+        {
+            result.Append($"{GetPropertyString(property, indentLevel + 1)}");
+        }
+
+        result.AppendLine($"{levelSpace}}}");
+
+        return result.ToString();
+    }
+
+    /// <summary>
+    /// プロパティエンティティからプロパティ文字列を作成して返す
+    /// </summary>
+    /// <param name="propertyEntity">プロパティエンティティインスタンス</param>
+    /// <param name="indentLevel">インデントレベル</param>
+    /// <returns>プロパティ文字列</returns>
+    private string GetPropertyString(Property propertyEntity, int indentLevel)
+    {
+        var result = new StringBuilder();
+
+        // デフォルト設定
+        var defaultValue = string.Empty;
+        if (propertyEntity.TypeName == "string" || propertyEntity.TypeName == "object")
+        {
+            defaultValue = "string.Empty";
+        }
+
+        // インデント設定
+        var levelSpace = new string('S', indentLevel).Replace("S", "  ");
+
+        // プロパティ文字列作成
+        result.Append($"{levelSpace}public {propertyEntity.TypeName} {propertyEntity.Name}{{set; get;}}");
+        if (!string.IsNullOrEmpty(defaultValue))
+        {
+            result.Append($" = {defaultValue};");
+        }
+        result.AppendLine();
+
+        return result.ToString();
+    }
+    #endregion
+
 }
