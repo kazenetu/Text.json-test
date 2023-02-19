c94fccfafe70ac82907db000ef1481005c040a72
kazenetu <rakudafactory@gmail.com>
2023-02-19 23:53:41 +0900

    [WIP]PropertyValueObjectを仮実装 #21

---
+++ src/console/Common/PropertyValueObject.cs
@@ -0,0 +1,72 @@
+/// <summary>
+/// プロパティValueObject
+/// </summary>
+public record PropertyValueObject
+{
+    /// <summary>
+    /// Class型名を返す
+    /// </summary>
+    /// <value>Class型名(未設定の場合はstring.Empty)</value>
+    public string PropertyTypeClassName
+    {
+        get
+        {
+            return Type?.ClassName ?? string.Empty;
+        }
+    }
+
+    /// <summary>
+    /// 名称
+    /// </summary>
+    /// <value>プロパティ名</value>
+    private string Name = string.Empty;
+
+    /// <summary>
+    /// 型ValueObject
+    /// </summary>
+    /// <value>型ValueObject</value>
+    private PropertyType? Type;
+
+    /// <summary>
+    /// 初期値
+    /// </summary>
+    /// <value>初期値</value>
+    private string DefaultValue = string.Empty;
+
+    /// <summary>
+    /// コンストラクタ
+    /// </summary>
+    /// <param name="name">クラス名称</param>
+    /// <param name="propertyType">型クラス インスタンス</param>
+    /// <returns>プロパティValueObject インスタンス</returns>
+    public PropertyValueObject(string name, PropertyType propertyType)
+    {
+        // パラメータチェック
+        if (string.IsNullOrEmpty(name)) throw new ArgumentException($"{nameof(name)} is null");
+
+        // デフォルト値設定
+        if (propertyType.ToString() is "string" or "object")
+        {
+            DefaultValue = "string.Empty";
+        }
+
+        // 名前とプロパティ型の設定
+        Name = name;
+        Type = propertyType;
+    }
+
+    /// <summary>
+    /// プロパティを返す
+    /// </summary>
+    /// <returns>C#プロパティ</returns>
+    public override string ToString()
+    {
+        var defualt = string.Empty;
+        if (DefaultValue is not "")
+        {
+            defualt = $" = {DefaultValue};";
+        }
+
+        return $"{Type} {Name}{{set; get;}}{defualt}";
+    }
+}
