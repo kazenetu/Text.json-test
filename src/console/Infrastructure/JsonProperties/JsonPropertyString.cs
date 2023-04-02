5a7570b7763e7a1de9cea7b7c6a522d1b76ceb59
kazenetu <rakudafactory@gmail.com>
2023-04-02 20:32:28 +0900

    string型のJsonPropertyを追加 #25

---
+++ src/console/Infrastructure/JsonProperties/JsonPropertyString.cs
@@ -0,0 +1,33 @@
+using System.Text.Json;
+using Domain.ValueObjects;
+
+namespace Infrastructure.JsonProperties;
+
+/// <summary>
+/// JSONプロパティ：string
+/// </summary>
+public class JsonPropertyString : IJsonProperty
+{
+    /// <summary>
+    /// キー名
+    /// </summary>
+    /// <returns>JsonValueKind</returns>
+    public JsonValueKind GetKeyName()
+    {
+        return JsonValueKind.String;
+    }
+
+    /// <summary>
+    /// Jsonプロパティ結果の取得
+    /// </summary>
+    /// <param name="element">対象インスタンス</param>
+    /// <param name="innerClassNo">インナークラス番号</param>
+    /// <returns>Jsonプロパティ結果</returns>
+    public JsonPropertyResult GetJsonPropertyResult(JsonProperty element, int innerClassNo)
+    {
+        // プロパティ生成
+        var propertyType = IJsonProperty.GetPropertyType(element.Value);
+        var prop =  new PropertyValueObject(element.Name, new PropertyType(propertyType, false));
+        return new JsonPropertyResult(prop, string.Empty, innerClassNo);
+    }
+}
