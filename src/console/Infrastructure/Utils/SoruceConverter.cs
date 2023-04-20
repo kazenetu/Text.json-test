6695e8cc30f4d5608252796ade7c16fca9a5f56a
kazenetu <rakudafactory@gmail.com>
2023-04-20 23:41:34 +0900

    ソースコンバーターのメソッドを仮実装 #27

--- src/console/Infrastructure/Utils/SoruceConverter.cs
+++ src/console/Infrastructure/Utils/SoruceConverter.cs
@@ -1,3 +1,5 @@
+using Domain.Entities;
+
 namespace Infrastructure.Utils;
 
 /// <summary>
@@ -5,5 +7,15 @@ namespace Infrastructure.Utils;
 /// </summary>
 internal static class SoruceConverter
 {
-
+    /// <summary>
+    /// C#コードを作成する
+    /// </summary>
+    /// <param name="classInstance">Class集約エンティティ</param>
+    /// <param name="namespaceName">名前空間</param>
+    /// <returns>C#コード</returns>
+    public static string ToCsCode(ClassesEntity classInstance, string? namespaceName = null)
+    {
+        // TODO コード作成
+        return string.Empty;
+    }
 }
