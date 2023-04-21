6a803382a72a038e734d1cc3de2655b28c2d8057
kazenetu <rakudafactory@gmail.com>
2023-04-21 23:22:28 +0900

    名前空間を実装 #27

--- src/console/Infrastructure/Utils/SoruceConverter.cs
+++ src/console/Infrastructure/Utils/SoruceConverter.cs
@@ -1,3 +1,4 @@
+using System.Text;
 using Domain.Entities;
 
 namespace Infrastructure.Utils;
@@ -11,11 +12,30 @@ internal static class SoruceConverter
     /// C#コードを作成する
     /// </summary>
     /// <param name="classInstance">Class集約エンティティ</param>
+    /// <param name="indentLevel">インデントレベル</param>
     /// <param name="namespaceName">名前空間</param>
     /// <returns>C#コード</returns>
-    public static string ToCsCode(ClassesEntity classInstance, string? namespaceName = null)
+    public static string ToCsCode(ClassesEntity classInstance, int indentLevel = 0, string? namespaceName = null)
     {
-        // TODO コード作成
-        return string.Empty;
+        var result = new StringBuilder();
+
+        if (!string.IsNullOrEmpty(namespaceName))
+        {
+            indentLevel++;
+            result.AppendLine($"namespace {namespaceName}");
+            result.AppendLine("{");
+        }
+
+        // インデント設定
+        var levelSpace = new string('S', indentLevel).Replace("S", "  ");
+
+        // TODO クラス生成
+
+        if (!string.IsNullOrEmpty(namespaceName))
+        {
+            result.AppendLine("}");
+        }
+
+        return result.ToString();
     }
 }
