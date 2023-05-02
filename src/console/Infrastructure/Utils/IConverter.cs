105682363736fee9d541a7b25f884190725c6d47
kazenetu <rakudafactory@gmail.com>
2023-05-02 23:52:28 +0900

    コード変更処理乗のパラメータをなしに変更 #28

--- src/console/Infrastructure/Utils/IConverter.cs
+++ src/console/Infrastructure/Utils/IConverter.cs
@@ -10,8 +10,6 @@ interface IConverter
     /// <summary>
     /// コード変更処理
     /// </summary>
-    /// <param name="classInstance">集約クラス</param>
-    /// <param name="param">オプションリスト</param>
     /// <returns>ソースコード文字列</returns>
-    string Convert(ClassesEntity classInstance, Dictionary<string, string> param);
+    string Convert();
 }
