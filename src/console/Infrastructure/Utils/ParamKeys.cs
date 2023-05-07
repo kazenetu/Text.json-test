f240a647a105b21f5fde8adfb624684d32b58345
kazenetu <rakudafactory@gmail.com>
2023-05-07 22:10:28 +0900

    パラメータキークラスを追加 #28

---
+++ src/console/Infrastructure/Utils/ParamKeys.cs
@@ -0,0 +1,12 @@
+namespace Infrastructure.Utils;
+
+/// <summary>
+/// パラメータキークラス
+/// </summary>
+static class ParamKeys
+{
+    /// <summary>
+    /// C#用名前空間
+    /// </summary>
+    public const string CS_NameSpace = "namespace";
+}
