d4ff93fd1b5b673d33347f6fe1f08fd8b6420ecb
kazenetu <rakudafactory@gmail.com>
2023-05-30 22:32:42 +0900

    C#ソースコードの入力クラスと出力用クラスを追加 #30

---
+++ Application/Commands/CSharpCommand.cs
@@ -0,0 +1,8 @@
+namespace Appplication.Commands;
+
+/// <summary>
+/// C#用コマンドクラス
+/// </summary>
+/// <param name="NameSpace">名前空間</param>
+/// <param name="RootPath">出力ルートパス</param>
+public record CSharpCommand(string NameSpace, string RootPath);
---
+++ Application/Models/CSharpResultModel.cs
@@ -0,0 +1,7 @@
+namespace Appplication.Models;
+
+/// <summary>
+/// 出力結果：C#ソースコード
+/// </summary>
+/// <param name="SorceCode"></param>
+public record CSharpResultModel(string SorceCode);
