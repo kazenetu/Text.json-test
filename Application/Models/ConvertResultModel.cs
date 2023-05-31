37fb465608a5801db942048352c6899a4ad7fc37
kazenetu <rakudafactory@gmail.com>
2023-05-31 23:40:38 +0900

    ソースコード変換結果モデルのファイル名変更 #30

--- Application/Models/ConvertResultModel.cs
+++ Application/Models/ConvertResultModel.cs
@@ -4,4 +4,4 @@ namespace Appplication.Models;
 /// ソースコード変換結果
 /// </summary>
 /// <param name="SorceCode">変換後のソースコード</param>
-public record CSharpResultModel(string SorceCode);
+public record ConvertResultModel(string SorceCode);
