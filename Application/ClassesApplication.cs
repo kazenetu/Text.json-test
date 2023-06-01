0861253d4782229976ccb5dafac2825d8f0a52b2
kazenetu <rakudafactory@gmail.com>
2023-06-01 22:11:21 +0900

    アプリケーションクラスのコンストラクタを実装 #30

--- Application/ClassesApplication.cs
+++ Application/ClassesApplication.cs
@@ -1,4 +1,29 @@
+using Appplication.Commands;
+using Appplication.Models;
+
 /// <summary>
 /// クラス集約アプリケーション
 /// </summary>
-public record ClassesApplication();
+public class ClassesApplication
+{
+    /// <summary>
+    /// Json解析リポジトリクラスインスタンス
+    /// </summary>
+    private IJsonRepository JsonRepository;
+
+    /// <summary>
+    /// ファイル出力リポジトリクラスインスタンス
+    /// </summary>
+    private IFileOutputRepository FileOutputRepository;
+
+    /// <summary>
+    /// コンストラクタ
+    /// </summary>
+    /// <param name="jsonRepository">Json解析リポジトリクラスインスタンス</param>
+    /// <param name="fileOutputRepository">ファイル出力リポジトリクラスインスタンス</param>
+    public ClassesApplication(IJsonRepository jsonRepository, IFileOutputRepository fileOutputRepository)
+    {
+        JsonRepository = jsonRepository;
+        FileOutputRepository = fileOutputRepository;
+    }
+}
