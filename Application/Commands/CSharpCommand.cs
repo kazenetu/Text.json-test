abe4cc0fc73e9350ac2a1db2cb9b6f691f7fd703
kazenetu <rakudafactory@gmail.com>
2023-06-03 22:25:24 +0900

    CS用コマンドクラスのソースコードプロパティを廃止、ルートクラス名を追加 #30

--- Application/Commands/CSharpCommand.cs
+++ Application/Commands/CSharpCommand.cs
@@ -5,4 +5,5 @@ namespace Appplication.Commands;
 /// </summary>
 /// <param name="NameSpace">名前空間</param>
 /// <param name="RootPath">出力ルートパス</param>
-public record CSharpCommand(string NameSpace, string RootPath);
+/// <param name="RootClassName">ルートクラス名(ファイル名)</param>
+public record CSharpCommand(string NameSpace, string RootPath,string RootClassName);
