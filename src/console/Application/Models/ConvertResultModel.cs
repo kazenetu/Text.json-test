c08fec601480789057df8779cbea29fa0f78c158
kazenetu <rakudafactory@gmail.com>
2023-06-10 22:01:48 +0900

    ConvertResultModelのクラスコメントのミスを修正 #31

--- src/console/Application/Models/ConvertResultModel.cs
+++ src/console/Application/Models/ConvertResultModel.cs
@@ -4,5 +4,5 @@ namespace Appplication.Models;
 /// ソースコード変換結果
 /// </summary>
 /// <param name="success">変換成功か否か</param>
-/// <param name="FileName">変換後のソースコード</param>
+/// <param name="FileName">出力ファイル名</param>
 public record ConvertResultModel(bool success, string FileName);
