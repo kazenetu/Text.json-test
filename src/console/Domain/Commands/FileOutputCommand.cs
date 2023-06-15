ac5140a74fd74ef742a1325ae3f828fbf7be24a1
kazenetu <rakudafactory@gmail.com>
2023-06-15 23:16:42 +0900

    recordに変更 #32

--- src/console/Domain/Commands/FileOutputCommand.cs
+++ src/console/Domain/Commands/FileOutputCommand.cs
@@ -3,26 +3,6 @@ namespace Domain.Commands;
 /// <summary>
 /// ファイル出力コマンドクラス
 /// </summary>
-public class FileOutputCommand
-{
-    /// <summary>
-    /// ファイル出力のルートパス
-    /// </summary>
-    public string RootPath { get; init; }
-
-    /// <summary>
-    /// 名前空間
-    /// </summary>
-    public string NameSpace { get; init; }
-
-    /// <summary>
-    /// コンストラクタ
-    /// </summary>
-    /// <param name="rootPath">ファイル出力のルートパス</param>
-    /// <param name="nameSpace">名前空間</param>
-    public FileOutputCommand(string rootPath, string nameSpace)
-    {
-        RootPath = rootPath;
-        NameSpace = nameSpace;
-    }
-}
+/// <param name="RootPath">ファイル出力のルートパス</param>
+/// <param name="NameSpace">名前空間</param>
+public record FileOutputCommand(string RootPath, string NameSpace);
