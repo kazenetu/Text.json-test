446c0431aaba781720009fc59d7837923c32ca52
kazenetu <rakudafactory@gmail.com>
2023-07-05 23:46:45 +0900

    インデントスペース数パラメータの追加 #37

--- src/console/Application/Commands/CSharpCommand.cs
+++ src/console/Application/Commands/CSharpCommand.cs
@@ -6,4 +6,5 @@ namespace Appplication.Commands;
 /// <param name="NameSpace">名前空間</param>
 /// <param name="RootPath">出力ルートパス</param>
 /// <param name="RootClassName">ルートクラス名(ファイル名)</param>
-public record CSharpCommand(string NameSpace, string RootPath,string RootClassName);
+/// <param name="indentSpaceCount">インデントスペース数</param>
+public record CSharpCommand(string NameSpace, string RootPath,string RootClassName, int indentSpaceCount);
