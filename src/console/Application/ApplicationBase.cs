ec2b5023970968f0a9b97218aca9e5fd6ae4b51b
kazenetu <rakudafactory@gmail.com>
2023-07-21 23:23:19 +0900

    アプリケーションスーパークラスを追加 #39

---
+++ src/console/Application/ApplicationBase.cs
@@ -0,0 +1,30 @@
+using TinyDIContainer;
+
+namespace Appplication;
+
+/// <summary>
+/// アプリケーションスーパークラス
+/// </summary>
+public abstract class ApplicationBase
+{
+    protected ApplicationBase()
+    {
+        // フィールドを検索
+        var filelds = this.GetType().GetFields(System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.NonPublic);
+        for(int i=0;i<filelds.Length;i++)
+        {
+            // インターフェイス以外は処理対象外
+            if(!filelds[i].FieldType.IsInterface) continue;
+
+            // 後ほど削除 インターフェイス情報を出力
+            Console.Write("=======>");
+            Console.WriteLine($"{filelds[i].FieldType.Name} {filelds[i].Name}");
+
+            // インターフェイスのインスタンスを作成
+            var instance = DIContainer.CreateInstance(filelds[i].FieldType);
+
+            // フィールドにインターフェイスインスタンスを設定
+            filelds[i].SetValue(this, instance);
+        }
+    }
+}
