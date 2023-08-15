b2ba135a0be9ee5484af0cfe967413f5e7a255db
kazenetu <rakudafactory@gmail.com>
2023-08-15 17:56:29 +0900

    複数言語が設定可能なファイル出力コマンドクラスを作成 #43

---
+++ src/console/Domain/Commands/FileCommand.cs
@@ -0,0 +1,32 @@
+namespace Domain.Commands;
+
+/// <summary>
+/// 出力言語タイプ
+/// </summary>
+public enum OutputLanguageType
+{
+    /// <summary>
+    /// C#
+    /// </summary>
+    CS, 
+}
+
+/// <summary>
+/// 追加パラメータ
+/// </summary>
+public enum ParamKeys
+{
+    /// <summary>
+    /// C#用名前空間
+    /// </summary>
+    CS_NameSpace,
+}
+
+/// <summary>
+/// ファイル出力コマンドクラス
+/// </summary>
+/// <param name="RootPath">ファイル出力のルートパス</param>
+/// <param name="LanguageType">出力言語タイプ</param>
+/// <param name="IndentSpaceCount">インデントスペース数</param>
+/// <param name="Params">追加パラメータ</param>
+public record FileCommand(string RootPath, OutputLanguageType LanguageType, int IndentSpaceCount, Dictionary<ParamKeys, string> Params);
