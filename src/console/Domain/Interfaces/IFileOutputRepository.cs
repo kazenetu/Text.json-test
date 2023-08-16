3f8b7d6bb8ed15eee7186dfe0b235e21557fbfa5
kazenetu <rakudafactory@gmail.com>
2023-08-16 15:28:09 +0900

    パラメータにFileCommandクラスを含むメソッドを仮実装 #43

--- src/console/Domain/Interfaces/IFileOutputRepository.cs
+++ src/console/Domain/Interfaces/IFileOutputRepository.cs
@@ -24,4 +24,16 @@ public interface IFileOutputRepository
     /// <param name="command">コマンドパラメータ</param>
     /// <returns>出力結果</returns>
     FileOutputResult OutputResult(ClassesEntity classInstance, FileOutputCommand command);
+
+    /// <summary>
+    /// ファイル出力する
+    /// </summary>
+    /// <param name="classInstance">集約エンティティ インスタンス</param>
+    /// <param name="command">コマンドパラメータ</param>
+    /// <returns>出力結果</returns>
+    FileOutputResult OutputResult(ClassesEntity classInstance, FileCommand command)
+    {
+        // TODO のちほど削除
+        throw new Exception();
+    }
 }
