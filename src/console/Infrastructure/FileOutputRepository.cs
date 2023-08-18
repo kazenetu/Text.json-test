666b82bbf46a80078a56e98afae662b541b8ff30
kazenetu <rakudafactory@gmail.com>
2023-08-18 23:02:18 +0900

    FileCommandをパラメータに含むOutputResultメソッドを実装 #43

--- src/console/Infrastructure/FileOutputRepository.cs
+++ src/console/Infrastructure/FileOutputRepository.cs
@@ -1,8 +1,8 @@
 using System.Text;
 using Domain.Commands;
 using Domain.Entities;
-using Domain.Results;
 using Domain.Interfaces;
+using Domain.Results;
 
 namespace Infrastructure;
 
@@ -91,6 +91,57 @@ public class FileOutputRepository : IFileOutputRepository
     /// <returns>出力結果</returns>
     public FileOutputResult OutputResult(ClassesEntity classInstance, FileCommand command)
     {
-        throw new NotImplementedException();
+        //必須パラメータチェック
+        if (classInstance is null) return new FileOutputResult(false, string.Empty, string.Empty);
+        if (command.RootPath is null) return new FileOutputResult(false, string.Empty, string.Empty);
+
+        // フォルダの存在確認とフォルダ作成
+        if (!Directory.Exists(command.RootPath))
+        {
+            Directory.CreateDirectory(command.RootPath);
+        }
+
+        // 拡張子取得
+        var ext = command.LanguageType switch
+        {
+            OutputLanguageType.CS => "cs",
+            _ => throw new Exception("ext error")
+        };
+
+        // ファイルパス作成
+        var filePath = Path.Combine(command.RootPath, $"{classInstance.Name}.{ext}");
+
+        // ソースコードを作成
+        var sourceCode = command.LanguageType switch
+        {
+            OutputLanguageType.CS => GetCSCode(),
+            _ => throw new Exception("ext error")
+        };
+
+        // ファイル出力
+        File.WriteAllText(filePath, sourceCode);
+
+        return new FileOutputResult(true, filePath, sourceCode);
+
+        // C#ソースコードを取得
+        string GetCSCode()
+        {
+            // 名前空間
+            var nameSpace = string.Empty;
+            if (command.Params.ContainsKey(ParamKeys.CS_NameSpace))
+            {
+                nameSpace = command.Params[ParamKeys.CS_NameSpace];
+            }
+
+            var initialSpaceIndex = 0;
+            // 名前空間が設定していない場合はインデントを調整する
+            if (nameSpace == string.Empty)
+            {
+                initialSpaceIndex = 1;
+            }
+
+            // Entityからソースコードの変換
+            return Utils.SoruceConverter.ToCsCode(classInstance, initialSpaceIndex, nameSpace, command.IndentSpaceCount);
+        }
     }
 }
