f2fb572369f2848b1c20fc841ceb549bd18160f7
kazenetu <rakudafactory@gmail.com>
2023-08-22 23:36:27 +0900

    インターフェイスの不要なメソッドを削除 #43

--- src/console/Domain/Interfaces/IFileOutputRepository.cs
+++ src/console/Domain/Interfaces/IFileOutputRepository.cs
@@ -15,21 +15,5 @@ public interface IFileOutputRepository
     /// <param name="classInstance">集約エンティティ インスタンス</param>
     /// <param name="command">コマンドパラメータ</param>
     /// <returns>出力結果</returns>
-    bool Output(ClassesEntity classInstance, FileOutputCommand command);
-
-    /// <summary>
-    /// ファイル出力する
-    /// </summary>
-    /// <param name="classInstance">集約エンティティ インスタンス</param>
-    /// <param name="command">コマンドパラメータ</param>
-    /// <returns>出力結果</returns>
-    FileOutputResult OutputResult(ClassesEntity classInstance, FileOutputCommand command);
-
-    /// <summary>
-    /// ファイル出力する
-    /// </summary>
-    /// <param name="classInstance">集約エンティティ インスタンス</param>
-    /// <param name="command">コマンドパラメータ</param>
-    /// <returns>出力結果</returns>
     FileOutputResult OutputResult(ClassesEntity classInstance, FileCommand command);
 }
