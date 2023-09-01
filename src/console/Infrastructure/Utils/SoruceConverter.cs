0d9c128656329a63a12cd07a2a5a484afe8d303b
kazenetu <rakudafactory@gmail.com>
2023-09-01 23:14:37 +0900

    Kotlinコードを作成するメソッドを追加 #43

--- src/console/Infrastructure/Utils/SoruceConverter.cs
+++ src/console/Infrastructure/Utils/SoruceConverter.cs
@@ -31,4 +31,25 @@ internal static class SoruceConverter
         var instance = CSConverter.Create(classInstance, param);
         return instance.Convert();
     }
+
+    /// <summary>
+    /// Kotlinコードを作成する
+    /// </summary>
+    /// <param name="classInstance">Class集約エンティティ</param>
+    /// <param name="packageName">パッケージ名</param>
+    /// <param name="indentSpaceCount">インデントスペース数</param>
+    /// <returns>C#コード</returns>
+    public static string ToKtCode(ClassesEntity classInstance, string? packageName = null, int indentSpaceCount = 2)
+    {
+        // パラメータ設定
+        var param = new Dictionary<ParamKeys, string>();
+        if (!string.IsNullOrEmpty(packageName))
+        {
+            param.Add(ParamKeys.KT_Package, packageName);
+        }
+        param.Add(ParamKeys.IndentSpaceCount, indentSpaceCount.ToString());
+
+        var instance = KTConverter.Create(classInstance, param);
+        return instance.Convert();
+    }
 }
