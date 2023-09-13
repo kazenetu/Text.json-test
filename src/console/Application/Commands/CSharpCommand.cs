0a2f9af79e06facd5bc453f7158937802120cac5
kazenetu <rakudafactory@gmail.com>
2023-09-13 21:46:46 +0900

    共通レコードクラスを継承する実装に変更 #47

--- src/console/Application/Commands/CSharpCommand.cs
+++ src/console/Application/Commands/CSharpCommand.cs
@@ -7,4 +7,6 @@ namespace Appplication.Commands;
 /// <param name="RootPath">出力ルートパス</param>
 /// <param name="RootClassName">ルートクラス名(ファイル名)</param>
 /// <param name="IndentSpaceCount">インデントスペース数</param>
-public record CSharpCommand(string NameSpace, string RootPath,string RootClassName, int IndentSpaceCount = 2);
+/// <param name="Prefix">固定プレフィックス</param>
+/// <param name="Suffix">固定サフィックス</param>
+public record CSharpCommand(string NameSpace, string RootPath,string RootClassName, int IndentSpaceCount = 2, string Prefix = "", string Suffix = "") :CommonCommand(Prefix, Suffix);
