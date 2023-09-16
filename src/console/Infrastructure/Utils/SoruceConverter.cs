d1d2cab1473c793f3b6a20351370801c75d12649
kazenetu <rakudafactory@gmail.com>
2023-09-16 22:04:56 +0900

    ユーティリティの生成メソッドのパラメータに固定プレフィックスと固定サフィックスを追加 #47

--- src/console/Infrastructure/Utils/SoruceConverter.cs
+++ src/console/Infrastructure/Utils/SoruceConverter.cs
@@ -17,8 +17,10 @@ internal static class SoruceConverter
     /// <param name="indentLevel">インデントレベル</param>
     /// <param name="namespaceName">名前空間</param>
     /// <param name="indentSpaceCount">インデントスペース数</param>
+    /// <param name="Prefix">固定プレフィックス</param>
+    /// <param name="Suffix">固定サフィックス</param>
     /// <returns>C#コード</returns>
-    public static string ToCsCode(ClassesEntity classInstance, int indentLevel = 0, string? namespaceName = null, int indentSpaceCount = 2)
+    public static string ToCsCode(ClassesEntity classInstance, int indentLevel = 0, string? namespaceName = null, int indentSpaceCount = 2, string Prefix = "", string Suffix = "")
     {
         // パラメータ設定
         var param = new Dictionary<ParamKeys,string>();
@@ -27,6 +29,8 @@ internal static class SoruceConverter
             param.Add(ParamKeys.CS_NameSpace, namespaceName);
         }
         param.Add(ParamKeys.IndentSpaceCount, indentSpaceCount.ToString());
+        param.Add(ParamKeys.Prefix, Prefix);
+        param.Add(ParamKeys.Suffix, Suffix);
 
         var instance = CSConverter.Create(classInstance, param);
         return instance.Convert();
@@ -38,8 +42,10 @@ internal static class SoruceConverter
     /// <param name="classInstance">Class集約エンティティ</param>
     /// <param name="packageName">パッケージ名</param>
     /// <param name="indentSpaceCount">インデントスペース数</param>
+    /// <param name="Prefix">固定プレフィックス</param>
+    /// <param name="Suffix">固定サフィックス</param>
     /// <returns>C#コード</returns>
-    public static string ToKtCode(ClassesEntity classInstance, string? packageName = null, int indentSpaceCount = 2)
+    public static string ToKtCode(ClassesEntity classInstance, string? packageName = null, int indentSpaceCount = 2, string Prefix = "", string Suffix = "")
     {
         // パラメータ設定
         var param = new Dictionary<ParamKeys, string>();
@@ -48,6 +54,8 @@ internal static class SoruceConverter
             param.Add(ParamKeys.KT_Package, packageName);
         }
         param.Add(ParamKeys.IndentSpaceCount, indentSpaceCount.ToString());
+        param.Add(ParamKeys.Prefix, Prefix);
+        param.Add(ParamKeys.Suffix, Suffix);
 
         var instance = KTConverter.Create(classInstance, param);
         return instance.Convert();
