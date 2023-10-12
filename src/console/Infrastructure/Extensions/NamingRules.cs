eadd945b1e60a96dcf61be906791b0008faf7f0f
kazenetu <rakudafactory@gmail.com>
2023-10-12 22:43:48 +0900

    Kotlin用変換クラスを追加 #54

--- src/console/Infrastructure/Extensions/NamingRules.cs
+++ src/console/Infrastructure/Extensions/NamingRules.cs
@@ -28,6 +28,49 @@ public static class NamingRules
     }
 
     /// <summary>
+    /// Kotlinクラス規約に変換する
+    /// </summary>
+    /// <param name="src">対象文字列</param>
+    /// <returns>UpperCaseの文字列</returns>
+    public static string ToKotlinClassNaming(this string src)
+    {
+        var result = ToUpperCase(src);
+        if (src.Contains('_', StringComparison.CurrentCulture))
+        {
+            var keywords = new StringBuilder();
+            foreach (var keyword in src.Split("_"))
+            {
+                keywords.Append(ToUpperCase(keyword));
+            }
+            result = keywords.ToString();
+        }
+        return result;
+    }
+
+    /// <summary>
+    /// Kotlinプロパティ規約に変換する
+    /// </summary>
+    /// <param name="src">対象文字列</param>
+    /// <returns>lowerCaseの文字列</returns>
+    public static string ToKotlinPrppertyNaming(this string src)
+    {
+        var result = ToLowerCase(src);
+        if (src.Contains('_', StringComparison.CurrentCulture))
+        {
+            var keywords = new StringBuilder();
+            foreach (var keyword in src.Split("_"))
+            {
+                if(keywords.Length <= 0)
+                    keywords.Append(ToLowerCase(keyword));
+                else
+                    keywords.Append(ToUpperCase(keyword));
+            }
+            result = keywords.ToString();
+        }
+        return result;
+    }
+
+    /// <summary>
     /// UpperCaseに変換する
     /// </summary>
     /// <param name="src">対象文字列</param>
@@ -39,6 +82,23 @@ public static class NamingRules
 
         var result = new StringBuilder();
         result.Append(src[..1].ToUpper());
+        if (src.Length > 1)
+            result.Append(src.AsSpan(1, src.Length - 1));
+        return result.ToString();
+    }
+
+    /// <summary>
+    /// lowerCaseに変換する
+    /// </summary>
+    /// <param name="src">対象文字列</param>
+    /// <returns>頭文字を大文字にした文字列</returns>
+    private static string ToLowerCase(string src)
+    {
+        if (string.IsNullOrEmpty(src))
+            return string.Empty;
+
+        var result = new StringBuilder();
+        result.Append(src[..1].ToLower());
         if (src.Length > 1)
             result.Append(src.AsSpan(1, src.Length - 1));
         return result.ToString();
